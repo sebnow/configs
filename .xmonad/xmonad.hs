6ab1a18f3a9a25af8987eedc66fc8d0ac68166e0
Sebastian Nowicki <sebnow@gmail.com>
2010-12-11 17:44:34 +0800

    xmonad: Add theme support
    Remove default keybindings (union customer keys instead)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,29 +1,60 @@
+import Data.List (intercalate)
 import XMonad
+import XMonad.Layout.Decoration (Theme(..), defaultTheme)
+import XMonad.Util.Themes (ThemeInfo(..))
 import qualified Data.Map as M
-import qualified XMonad.StackSet as StackSet
 
 myConfig = defaultConfig
     { terminal           = "urxvtc"
-    , normalBorderColor  = "#111111"
-    , focusedBorderColor = "#555555"
+    , normalBorderColor  = inactiveBorderColor myTheme
+    , focusedBorderColor = activeBorderColor myTheme
     , borderWidth        = 1
     , modMask            = mod4Mask
     , workspaces         = ["www", "chat"] ++ map show [3 .. 9]
     , keys               = \c -> myKeys c `M.union` keys defaultConfig c
     }
 
+-- Key bindings {{{
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
-    [ ((modMask, xK_Return),          spawn $ XMonad.terminal conf)
-    , ((modMask, xK_p),               spawn "dmenu_run") -- program launcher
-    , ((modMask, xK_j),               windows StackSet.focusDown)
-    , ((modMask, xK_k),               windows StackSet.focusUp)
-    , ((modMask, xK_h),               sendMessage Shrink)
-    , ((modMask, xK_l),               sendMessage Expand)
-    , ((modMask .|. shiftMask, xK_j), windows StackSet.swapDown)
-    , ((modMask .|. shiftMask, xK_k), windows StackSet.swapUp)
-    , ((modMask .|. shiftMask, xK_x), kill)
-    , ((modMask .|. shiftMask, xK_r), restart "xmonad" True)
+    [ ((modMask, xK_p), spawn $ themedDmenu myTheme) -- program launcher
     ]
+-- }}}
 
-main = xmonad $ myConfig
+-- Themes {{{
+tangoTheme :: ThemeInfo
+tangoTheme = TI
+    { themeName        = "Tango!"
+    , themeAuthor      = "Sebastian Nowicki"
+    , themeDescription = "Tango Project color scheme"
+    , theme = defaultTheme
+        { activeColor         = "#2e3436"
+        , activeTextColor     = "#eeeeec"
+        , activeBorderColor   = "#555753"
+        , inactiveColor       = "#555753"
+        , inactiveTextColor   = "#d3d7cf"
+        , inactiveBorderColor = "#2e3436"
+        , urgentColor         = "#f57900"
+        , urgentTextColor     = "#fcaf3e"
+        , urgentBorderColor   = "#ce5c00"
+        }
+    }
+
+myTheme :: Theme
+myTheme = (theme tangoTheme)
+    { fontName = "-*-terminus-medium-*-*-*-*-*-*-*-*-*-*-*"
+    }
+-- }}}
+
+-- Return dmenu cmd specifying an XMonad theme
+themedDmenu t = intercalate " " cmd
+    where cmd = [ "dmenu_run"
+                , "-fn", q $ fontName t
+                , "-nb", q $ inactiveColor t
+                , "-nf", q $ inactiveTextColor t
+                , "-sb", q $ activeColor t
+                , "-sf", q $ activeTextColor t
+                ]
+          q s = "\"" ++ s ++ "\"" -- Quote string
+
+main = xmonad myConfig
 
