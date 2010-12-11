002c5a3a31447ac508dcc5df01541d603512cf57
Sebastian Nowicki <sebnow@gmail.com>
2010-12-11 16:55:47 +0800

    xmonad: append default keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,28 +1,29 @@
 import XMonad
-import XMonad.Util.Run(spawnPipe)
-import qualified Data.Map as Map
+import qualified Data.Map as M
 import qualified XMonad.StackSet as StackSet
 
-myConfig = defaultConfig {
-	terminal = "urxvtc"
-	, borderWidth = 1
-	, modMask = mod4Mask
-	, workspaces = ["www", "chat", "3", "4", "5"]
-	, keys = myKeys
-}
+myConfig = defaultConfig
+    { terminal           = "urxvtc"
+    , normalBorderColor  = "#111111"
+    , focusedBorderColor = "#555555"
+    , borderWidth        = 1
+    , modMask            = mod4Mask
+    , workspaces         = ["www", "chat"] ++ map show [3 .. 9]
+    , keys               = \c -> myKeys c `M.union` keys defaultConfig c
+    }
 
-myKeys conf@(XConfig {XMonad.modMask = modMask}) = Map.fromList $ [
-	  ((modMask, xK_Return), spawn $ XMonad.terminal conf)
-	, ((modMask, xK_p), spawn "dmenu_run") -- program launcher
-	, ((modMask, xK_j), windows StackSet.focusDown)
-	, ((modMask, xK_k), windows StackSet.focusUp)
-	, ((modMask, xK_h), sendMessage Shrink)
-	, ((modMask, xK_l), sendMessage Expand)
-	, ((modMask .|. shiftMask, xK_j), windows StackSet.swapDown)
-	, ((modMask .|. shiftMask, xK_k), windows StackSet.swapUp)
-	, ((modMask .|. shiftMask, xK_x), kill)
-	, ((modMask .|. shiftMask, xK_r), restart "xmonad" True)
-	]
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
+    [ ((modMask, xK_Return),          spawn $ XMonad.terminal conf)
+    , ((modMask, xK_p),               spawn "dmenu_run") -- program launcher
+    , ((modMask, xK_j),               windows StackSet.focusDown)
+    , ((modMask, xK_k),               windows StackSet.focusUp)
+    , ((modMask, xK_h),               sendMessage Shrink)
+    , ((modMask, xK_l),               sendMessage Expand)
+    , ((modMask .|. shiftMask, xK_j), windows StackSet.swapDown)
+    , ((modMask .|. shiftMask, xK_k), windows StackSet.swapUp)
+    , ((modMask .|. shiftMask, xK_x), kill)
+    , ((modMask .|. shiftMask, xK_r), restart "xmonad" True)
+    ]
 
 main = xmonad $ myConfig
 
