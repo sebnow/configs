cdfa749aa2ff8348b153e9cd77cef91ff83bfdf3
Sebastian Nowicki <sebnow@gmail.com>
2010-12-20 20:46:02 +0800

    xmonad: Add dzen dynamic log

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,8 @@
-import Data.List (intercalate)
+import qualified Data.Map as M
 import XMonad
+import XMonad.Hooks.DynamicLog hiding (dzen)
 import XMonad.Layout.Decoration (Theme(..), defaultTheme)
 import XMonad.Util.Themes (ThemeInfo(..))
-import qualified Data.Map as M
 
 myConfig = defaultConfig
     { terminal           = "urxvtc"
@@ -14,10 +14,16 @@ myConfig = defaultConfig
     , keys               = \c -> myKeys c `M.union` keys defaultConfig c
     }
 
+trayerWidth :: (Integral a) => a
+trayerWidth = 48
+
 -- Key bindings {{{
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
     [ ((modMask, xK_p), spawn $ themedDmenu myTheme) -- program launcher
     ]
+
+toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
+toggleStrutsKey XConfig{modMask = modMask} = (modMask, xK_b)
 -- }}}
 
 -- Themes {{{
@@ -46,15 +52,62 @@ myTheme = (theme tangoTheme)
 -- }}}
 
 -- Return dmenu cmd specifying an XMonad theme
-themedDmenu t = intercalate " " cmd
-    where cmd = [ "dmenu_run"
-                , "-fn", q $ fontName t
-                , "-nb", q $ inactiveColor t
-                , "-nf", q $ inactiveTextColor t
-                , "-sb", q $ activeColor t
-                , "-sf", q $ activeTextColor t
-                ]
-          q s = "\"" ++ s ++ "\"" -- Quote string
+themedDmenu :: Theme -> String
+themedDmenu t = unwords
+    [ "dmenu_run"
+    , "-fn", quote $ fontName t
+    , "-nb", quote $ inactiveColor t
+    , "-nf", quote $ inactiveTextColor t
+    , "-sb", quote $ activeColor t
+    , "-sf", quote $ activeTextColor t
+    ]
 
-main = xmonad myConfig
+data Alignment = AlignLeft | AlignCenter | AlignRight
+
+dzenCmd :: Theme -> Alignment -> String
+dzenCmd t a = unwords
+    [ "dzen2"
+    , "-e", "'onstart=lower'"
+    , "-h", show $ decoHeight t
+    , "-ta", case a of
+        AlignLeft -> "l"
+        AlignCenter -> "c"
+        AlignRight -> "r"
+    , "-fn", quote $ fontName t
+    , "-bg", quote $ inactiveColor t
+    , "-fg", quote $ inactiveTextColor t
+    , "-w", "839"
+    ]
+
+trayer :: (Integral a) => Theme -> a -> String
+trayer t w = unwords
+    [ "trayer"
+    , "--SetPartialStrut true"
+    , "--widthtype request"
+    , "--transparent true"
+    , "--alpha 0"
+    , "--tint", "0x" ++ tail (inactiveColor t)
+    , "--expand true"
+    , "--height", show (decoHeight t)
+    , "--width", show w
+    , "--edge top"
+    , "--align right"
+    ]
+
+quote :: String -> String
+quote s = "'" ++ s ++ "'"
+
+dzenPP' :: Theme -> PP
+dzenPP' t = dzenPP
+    { ppCurrent         = dzenColor (activeTextColor t) (activeColor t) . pad
+    , ppHidden          = ppInactive
+    , ppHiddenNoWindows = ppInactive
+    , ppUrgent          = dzenColor (urgentTextColor t) (urgentColor t) . dzenStrip
+    , ppTitle           = dzenColor (activeTextColor t) (activeColor t) . pad . shorten 80
+    , ppLayout          = ppInactive
+    } where ppInactive = dzenColor (inactiveTextColor t) (inactiveColor t) . pad
+
+dzen t = statusBar (dzenCmd t AlignLeft) (dzenPP' t) toggleStrutsKey
+
+main = spawn (trayer myTheme trayerWidth) >> dzen myTheme myConfig >>= xmonad
 
