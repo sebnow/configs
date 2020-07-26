5e39bb83dd55f517de7c6c0439954b6746335a3c
Sebastian Nowicki <sebnow@gmail.com>
2020-07-26 13:31:20 +0200

    emacs: Switch to evil mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,11 +10,34 @@
 (require 'package)
 (add-to-list 'package-archives
 			 '("melpa" . "https://melpa.org/packages/") t)
-
 (package-initialize)
+(package-refresh-contents)
 
-(setq viper-mode t)
-(require 'viper)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+(unless (package-installed-p 'ayu-theme)
+  (package-install 'ayu-theme))
 
 (use-package ayu-theme
   :config (load-theme 'ayu-dark t))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ayu-theme evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
