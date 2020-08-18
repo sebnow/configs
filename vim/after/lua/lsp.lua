5e6be1c6844c9d15870b1b3240b3e2521e19da15
Sebastian Nowicki <sebnow@gmail.com>
2020-08-18 11:34:00 +0200

    vim: Configure YAML Language Server

--- vim/after/lua/lsp.lua
+++ vim/after/lua/lsp.lua
@@ -1,9 +1,11 @@
 local ncm2 = require('ncm2')
 local nvim_lsp = require('nvim_lsp')
 
-nvim_lsp.rls.setup({on_init=ncm2.register_lsp_source})
-nvim_lsp.gopls.setup({on_init=ncm2.register_lsp_source})
-nvim_lsp.flow.setup({on_init=ncm2.register_lsp_source})
+local opts = {on_init=ncm2.register_lsp_source}
+nvim_lsp.rls.setup(opts)
+nvim_lsp.gopls.setup(opts)
+nvim_lsp.flow.setup(opts)
+nvim_lsp.yamlls.setup(opts)
 nvim_lsp.terraformls.setup({
 	on_init = ncm2.register_lsp_source,
 	filetypes = {"terraform", "tf"},
