878513eed43d0b451e01ac7b8115a4ce6ffb71cb
Sebastian Nowicki <sebnow@gmail.com>
2021-01-19 21:00:32 +0100

    vim(telescope): Use vim buffer previewers

--- vim/lua/sebnow/telescope.lua
+++ vim/lua/sebnow/telescope.lua
@@ -10,5 +10,8 @@ telescope.setup({
             },
         },
         file_sorter = require('telescope.sorters').get_fzy_sorter,
+        file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+        grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
     }
 })
