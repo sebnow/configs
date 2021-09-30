3cbe029b82da86eea92898e9601325602acec8b1
Sebastian Nowicki <sebnow@gmail.com>
2021-09-30 18:13:16 +0200

    vim: Enable all maintained treesitter languages

--- vim/lua/sebnow/treesitter.lua
+++ vim/lua/sebnow/treesitter.lua
@@ -1,7 +1,7 @@
-local treesitter = require('nvim-treesitter.configs')
+local config = require("nvim-treesitter.configs")
 
-treesitter.setup({
-	ensure_installed = {"go", "rust", "bash", "css", "graphql", "html", "json", "javascript", "nix", "typescript", "lua", "yaml", "toml"},
+config.setup({
+	ensure_installed = "maintained",
 	highlight = {
 		enable = true,
 	},
