fe973b47dc0dd3e98315a9b000bf129b7d25430d
Sebastian Nowicki <sebnow@gmail.com>
2022-06-16 14:52:42 +0200

    style(vim): Reformat cmp config

--- vim/lua/sebnow/plugins/cmp.lua
+++ vim/lua/sebnow/plugins/cmp.lua
@@ -1,25 +1,26 @@
 local cmp = require("cmp")
 
 cmp.setup({
-	mapping = {
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<C-y>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-	},
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "path" },
-		{ name = "ultisnips" },
-		{ name = "buffer", keyword_length = 4 },
-	},
-	snippet = {
-		expand = function(args)
-			vim.fn["UltiSnips#Anon"](args.body)
-		end,
-	},
+  mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-y>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+  },
+  sources = {
+    { name = "buffer", keyword_length = 4 },
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "ultisnips" },
+    { name = "luasnip" },
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
 })
