eec3bae286de181e9326c0d88dda4a15ee67a533
Sebastian Nowicki <sebnow@gmail.com>
2024-02-12 22:48:59 +0100

    vim: Add macro recording indicator to lualine

--- nix/home-manager/neovim/lua/sebnow/ui.lua
+++ nix/home-manager/neovim/lua/sebnow/ui.lua
@@ -7,6 +7,19 @@ require("lualine").setup({
     component_separators = "|",
     section_separators = "",
   },
+  sections = {
+    lualine_a = {
+      "mode",
+      function()
+        local reg = vim.fn.reg_recording()
+        if reg == "" then
+          return ""
+        else
+          return "REC @" .. reg
+        end
+      end,
+    },
+  },
   extensions = { "fugitive", "quickfix" },
 })
 
@@ -31,3 +44,33 @@ wk.register({
   c = { name = "Code Actions" },
   f = { name = "Format" },
 }, { prefix = "<localleader>", mode = { "n", "v" } })
+
+-- Ripped off from https://www.reddit.com/r/neovim/comments/xy0tu1/comment/irfegvd/
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    require("lualine").refresh({
+      place = { "statusline" },
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    -- This is going to seem really weird!
+    -- Instead of just calling refresh we need to wait a moment because of the nature of
+    -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+    -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+    -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+    -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+    local timer = vim.loop.new_timer()
+    timer:start(
+      50,
+      0,
+      vim.schedule_wrap(function()
+        require("lualine").refresh({
+          place = { "statusline" },
+        })
+      end)
+    )
+  end,
+})
