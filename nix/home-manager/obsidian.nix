0ad9b3b1f166be1040774968fa808d26cb88cbc2
Sebastian Nowicki <sebnow@sebastians-mbp.home>
2024-12-06 15:05:12 +0100

    obsidian: Apply Linux font overrides only on Linux

--- nix/home-manager/obsidian.nix
+++ nix/home-manager/obsidian.nix
@@ -3,13 +3,15 @@
   pkgs,
   ...
 }: {
-  home.packages = [
-    (pkgs.obsidian.overrideAttrs (prev: rec {
-      desktopItem = prev.desktopItem.override {
-        # https://pandasauce.org/post/linux-fonts/#application-settings
-        exec = "obsidian --disable-font-subpixel-positioning %U";
-      };
-      installPhase = builtins.replaceStrings ["${prev.desktopItem}"] ["${desktopItem}"] prev.installPhase;
-    }))
-  ];
+  home.packages =
+    if pkgs.stdenv.isLinux
+    then
+      (pkgs.obsidian.overrideAttrs (prev: rec {
+        desktopItem = prev.desktopItem.override {
+          # https://pandasauce.org/post/linux-fonts/#application-settings
+          exec = "obsidian --disable-font-subpixel-positioning %U";
+        };
+        installPhase = builtins.replaceStrings ["${prev.desktopItem}"] ["${desktopItem}"] prev.installPhase;
+      }))
+    else [pkgs.obsidian];
 }
