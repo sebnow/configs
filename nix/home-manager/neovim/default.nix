3fc978f89866bd8829be19e3c84eb5c31ca9563e
Sebastian Nowicki <sebnow@gmail.com>
2025-03-30 13:01:05 +0200

    vim: Consolidate config to a single file
    It's starting to get hard to make clear boundaries between the
    files. Just dump it all into one file and try to clean it up later.

--- nix/home-manager/neovim/default.nix
+++ nix/home-manager/neovim/default.nix
@@ -3,23 +3,55 @@
   pkgs,
   ...
 }: {
-  imports = [
-    ./completion.nix
-    ./git.nix
-    ./language.nix
-    ./telescope.nix
-    ./ui.nix
-  ];
-
   config = {
     programs.neovim = {
       enable = true;
       defaultEditor = true;
-      extraLuaConfig = "require('sebnow.settings')";
-    };
-
-    xdg.configFile."nvim/lua/sebnow/settings.lua" = {
-      source = ./lua/sebnow/settings.lua;
+      extraLuaConfig = builtins.readFile ./config.lua;
+      plugins = with pkgs.vimPlugins; [
+        catppuccin-nvim
+        comment-nvim
+        conform-nvim
+        go-nvim
+        indent-blankline-nvim
+        lualine-nvim
+        markview-nvim
+        neoscroll-nvim
+        noice-nvim
+        nvim-lspconfig
+        nvim-treesitter-context
+        nvim-treesitter.withAllGrammars
+        nvim-web-devicons
+        oil-nvim
+        rust-tools-nvim # TODO: Review - may not be required
+        telescope-nvim
+        telescope-ui-select-nvim
+        trouble-nvim
+        vim-fugitive
+        vim-matchup
+        vim-terraform
+        which-key-nvim
+        which-key-nvim
+      ];
+      extraPackages = with pkgs; [
+        fd
+        golangci-lint-langserver
+        gopls
+        jsonnet-language-server
+        lua-language-server
+        marksman
+        nixd
+        nodePackages.bash-language-server
+        nodePackages.prettier
+        nodePackages.typescript-language-server
+        nodePackages.vscode-langservers-extracted
+        nodePackages.yaml-language-server
+        python311Packages.python-lsp-server
+        rust-analyzer
+        shellcheck
+        stylua
+        terraform-ls
+      ];
     };
   };
 }
--- nix/home-manager/neovim/lua/sebnow/completion.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client ~= nil and client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
