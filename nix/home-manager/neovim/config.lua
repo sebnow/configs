a9b6fa2aa541f34ac67dc6f9b84d583bee2f7a52
Sebastian Nowicki <sebnow@gmail.com>
2025-09-18 18:10:52 +0200

    vim: Enable LSP doc borders

--- nix/home-manager/neovim/config.lua
+++ nix/home-manager/neovim/config.lua
@@ -133,7 +133,7 @@ require("noice").setup({
     command_palette = false,
     long_message_to_split = true,
     inc_rename = true,
-    lsp_doc_border = false,
+    lsp_doc_border = true,
   },
 })
 
