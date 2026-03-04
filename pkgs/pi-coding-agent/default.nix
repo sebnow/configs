963fe7feac0ed47d8149a208d2e8ade4a8fc0822
Sebastian Nowicki <sebnow@gmail.com>
2026-03-04 21:33:19 +0000

    pkgs(pi-coding-agent): update to 0.56.1

--- pkgs/pi-coding-agent/default.nix
+++ pkgs/pi-coding-agent/default.nix
@@ -8,14 +8,14 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "pi-coding-agent";
-  version = "0.55.1";
+  version = "0.56.1";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-${finalAttrs.version}.tgz";
-    hash = "sha256-Qjuv8GzMdENnNwX5xo/Yfh4aRhn7N7pIlXrkeoHGJMk=";
+    hash = "sha256-p454mM9G8v0hUgy8D+DHnFQD2s5PiYjX+nQuUXGQuNU=";
   };
 
-  npmDepsHash = "sha256-sxFRcIT2FPn/7lb8KZ7/ivMCpdv5QqWFsbIQHU6p9RE=";
+  npmDepsHash = "sha256-imC8KuY3u0NVmm6hcL/DQUr4Duhw6ry3T5X+86UcPcY=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
