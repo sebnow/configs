28575f79f7b8d814b367e8d63246b15c360febd9
Sebastian Nowicki <sebnow@gmail.com>
2026-05-30 12:33:17 +0200

    nix: lock to 26.05 releases

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Home Manager configuration of sebnow";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     catppuccin = {
