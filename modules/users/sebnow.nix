ae8d7d90374b0a9bed8117f20f927ad77e924a8f
Sebastian Nowicki <sebnow@gmail.com>
2026-07-04 13:44:07 +0200

    nix: re-enable bash integration

--- modules/users/sebnow.nix
+++ modules/users/sebnow.nix
@@ -87,6 +87,7 @@ in
         programs.direnv = {
           enable = true;
           nix-direnv.enable = true;
+          enableBashIntegration = true;
         };
 
         targets.genericLinux = {
